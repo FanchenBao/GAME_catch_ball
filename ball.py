c5e1a44b906153e82b9b5c4676533ad774fe476f
Fanchen <jonathonbao@gmail.com>
2018-02-10 23:02:31 -0500

    basket can move within boundary of the screen

--- ball.py
+++ ball.py
@@ -1,4 +1,30 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ball():
-	
+
+class Ball(Sprite):
+	''' a class to mamage all balls'''
+	def __init__(self, screen, ai_settings):
+		super().__init__()
+		# initialize ball and screen
+		self.screen = screen
+		self.ai_settings = ai_settings
+		# load images and get rect
+		self.image = pygame.image.load('images/ball.bmp')
+		self.rect = self.image.get_rect()
+		self.screen_rect = self.screen.get_rect()
+		# default ball starting position is the top left corner of screen
+		self.rect.x = 0
+		self.rect.y = 0
+		self.y = float(self.rect.y)
+
+		self.ball_speed = self.ai_settings.ball_speed
+
+	def update(self):
+	'''ball always goes down'''
+		self.y += self.ball_speed
+		self.rect.y = self.y
+
+	def blitme(self):
+	''' draw the ball'''
+		self.screen.blit(self.image, self.rect)
